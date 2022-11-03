87b2c13f99a0307ddc7f9d81c6414c43be82c774
Felisha <felisha.j.yu@gmail.com>
2022-11-03 16:02:39 -0700

    adding starter code

---
+++ models/index.js
@@ -0,0 +1,28 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+
+// Categories have many Products
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'product_id',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'tag_id',
+});
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
