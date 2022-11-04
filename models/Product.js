dadee7b636510d3834f218df221f6ad63add1981
Felisha <felisha.j.yu@gmail.com>
2022-11-03 20:21:28 -0700

    updated models to get seeds to work properly

--- models/Product.js
+++ models/Product.js
@@ -34,6 +34,13 @@ Product.init(
       validate: {
         isNumeric: true
       }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
--- models/index.js
+++ models/index.js
@@ -5,9 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category)
+Product.belongsTo(Category,{
+
+  foreignKey: 'category_id',
+});
 // Categories have many Products
-Category.hasMany(Product)
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+})
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
